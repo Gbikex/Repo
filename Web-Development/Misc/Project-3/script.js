7693150c48026df1cf04bb8bf283c0eff8876def
Gbikex <csatgabor@gmail.com>
2025-05-01 19:56:45 +0200

    Change: minor rewroking of the variables game lose and score skeleton added

--- Web-Development/Misc/Project-3/script.js
+++ Web-Development/Misc/Project-3/script.js
@@ -6,7 +6,14 @@ const responses = {
   high: "Your guess is too high!",
   low: "Your guess is too low!",
   win: "You guessed the number, we have a winner!",
+  lost: "Ran out of chances, you lost the game",
 };
+const formDisable = document.getElementById("guess--number");
+const btnSubmitDisable = document.querySelector(".submit");
+let guessChance = 20;
+document.querySelector(".left").textContent = guessChance;
+
+//TO DO SCORE NOT WORKS
 
 console.log(randomNumber);
 
@@ -14,18 +21,29 @@ const btnSubmit = document
   .querySelector(".submit")
   .addEventListener("click", () => {
     const guess = document.getElementById("guess--number").value;
-    console.log(typeof guess);
-    if (guess > 0 && guess < 20) {
-      if (guess < randomNumber) {
-        responseText.textContent = responses.low;
-      } else if (guess > randomNumber) {
-        responseText.textContent = responses.high;
+
+    if (guessChance > 0) {
+      if (guess > 0 && guess <= 20) {
+        if (guess < randomNumber) {
+          responseText.textContent = responses.low;
+          document.querySelector(".left").textContent = guessChance--;
+        } else if (guess > randomNumber) {
+          responseText.textContent = responses.high;
+          document.querySelector(".left").textContent = guessChance--;
+        } else {
+          responseText.textContent = responses.win;
+
+          document.querySelector(".result").textContent = randomNumber;
+
+          formDisable.disabled = true;
+          btnSubmitDisable.disabled = true;
+        }
       } else {
-        responseText.textContent = responses.win;
-        document.querySelector(".submit").disabled = true;
-        document.getElementById("guess--number").disabled = true;
+        alert("Please enter a valid number between 1 and 20!");
       }
     } else {
-      alert("Please enter a valid number between 1 and 20!");
+      formDisable.disabled = true;
+      btnSubmitDisable.disabled = true;
+      responseText.textContent = responses.lost;
     }
   });
