922aa79eb132811204b4aed6e24e87ff67eb21f4
Gbikex <csatgabor@gmail.com>
2025-05-01 11:40:15 +0200

    Add: guess check with response on the page

--- Web-Development/Misc/Project-3/script.js
+++ Web-Development/Misc/Project-3/script.js
@@ -2,19 +2,25 @@
 
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(randomNumber);
+let responseText = document.querySelector(".response");
+
+const responses = {
+  high: "Your guess is too high!",
+  low: "Your guess is too low!",
+  win: "You guessed the number, we have a winner!",
+};
 
 const btnSubmit = document
   .querySelector(".btn-submit")
   .addEventListener("click", () => {
-    console.log("Submit has been clicked");
     const guess = document.getElementById("guess--number").value;
     console.log(guess);
 
     if (guess < randomNumber) {
-      console.log("Your guess is to low");
+      responseText.textContent = responses.low;
     } else if (guess > randomNumber) {
-      console.log("Your guess is to high");
+      responseText.textContent = responses.high;
     } else {
-      console.log("You won");
+      responseText.textContent = responses.win;
     }
   });
