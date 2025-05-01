a91a14b350b01b213b503797401cf417dfd5a7ff
Gbikex <csatgabor@gmail.com>
2025-05-01 11:57:57 +0200

    Add play logic and disable elements

--- Web-Development/Misc/Project-3/script.js
+++ Web-Development/Misc/Project-3/script.js
@@ -1,26 +1,31 @@
 "use strict";
 
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomNumber);
 let responseText = document.querySelector(".response");
-
 const responses = {
   high: "Your guess is too high!",
   low: "Your guess is too low!",
   win: "You guessed the number, we have a winner!",
 };
 
+console.log(randomNumber);
+
 const btnSubmit = document
-  .querySelector(".btn-submit")
+  .querySelector(".submit")
   .addEventListener("click", () => {
     const guess = document.getElementById("guess--number").value;
-    console.log(guess);
-
-    if (guess < randomNumber) {
-      responseText.textContent = responses.low;
-    } else if (guess > randomNumber) {
-      responseText.textContent = responses.high;
+    console.log(typeof guess);
+    if (guess > 0 && guess < 20) {
+      if (guess < randomNumber) {
+        responseText.textContent = responses.low;
+      } else if (guess > randomNumber) {
+        responseText.textContent = responses.high;
+      } else {
+        responseText.textContent = responses.win;
+        document.querySelector(".submit").disabled = true;
+        document.getElementById("guess--number").disabled = true;
+      }
     } else {
-      responseText.textContent = responses.win;
+      alert("Please enter a valid number between 1 and 20!");
     }
   });
