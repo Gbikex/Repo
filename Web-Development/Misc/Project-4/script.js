ecf6da684df20cb951224a563d71c2a5d99fcd2d
Gbikex <csatgabor@gmail.com>
2025-05-07 21:58:02 +0200

    Add:skeleton of the pw gen function

--- Web-Development/Misc/Project-4/script.js
+++ Web-Development/Misc/Project-4/script.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log("Test");
+function generatePassword(pwLong = 11) {
+  const charList = ["a", "b", "c", "e", "e", "f", "g", "h"];
+  const numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const symList = ["§", "'", "+", "!", "%", "/", "=", "("];
+
+  let numOpt = 0;
+
+  pwLong % 3 === 0 ? (numOpt = pwLong / 3) : (numOpt = pwLong / 2);
+  console.log(numOpt);
+
+  const charLength = Math.floor(numOpt);
+  const numLength = Math.floor(numOpt);
+  const symLength = pwLong - numOpt;
+
+  console.log(charLength, numLength, symLength);
+}
+
+generatePassword(undefined);
