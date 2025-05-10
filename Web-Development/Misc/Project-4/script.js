744472e05199c5a46f476165a145fe3752e9e292
Gbikex <csatgabor@gmail.com>
2025-05-10 11:23:29 +0200

    Add: copy button create and pw copy to clipboard functionality

--- Web-Development/Misc/Project-4/script.js
+++ Web-Development/Misc/Project-4/script.js
@@ -1,11 +1,20 @@
 "use strict";
 
 const btnGenerate = document.querySelector(".generate");
-const inputLength = document.querySelector(".pw.length");
-const pDisplay = document.querySelector(".display");
+const btnReset = document.querySelector(".reset");
+const btnCopy = document.createElement("button");
+const btnCopyText = document.createTextNode("Copy");
+let inputLength = document.querySelector(".pw.length");
+let pDisplay = document.querySelector(".display.pw");
+const pMessage = document.querySelector(".display.msg");
+const responseMsg = {
+  genPw: "Here is your password:",
+};
 console.log(btnGenerate);
+console.log(btnReset);
 console.log(inputLength);
 console.log(pDisplay);
+console.log(pMessage);
 
 function generatePassword(pwLong = 16) {
   let newPassword;
@@ -57,5 +66,22 @@ btnGenerate.addEventListener("click", () => {
   console.log("Test");
   console.log(inputLength.value);
   let pwDisplay = generatePassword(inputLength.value || undefined);
+  pMessage.textContent = responseMsg.genPw;
   pDisplay.textContent = pwDisplay;
+  btnCopy.appendChild(btnCopyText);
+  pDisplay.appendChild(btnCopy);
+});
+
+btnCopy.addEventListener("click", () => {
+  console.log("Copy");
+  let pwToCopy = document.querySelector(".display.pw").textContent;
+  pwToCopy = pwToCopy.replace("Copy", "");
+  navigator.clipboard.writeText(pwToCopy);
+  console.log(pwToCopy);
+});
+
+btnReset.addEventListener("click", () => {
+  pMessage.textContent = "";
+  pDisplay.textContent = "";
+  inputLength.value = "";
 });
