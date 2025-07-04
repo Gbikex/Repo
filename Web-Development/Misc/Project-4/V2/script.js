85ecdaff22bf99bf3574e3a8138dd0624ec1e165
Gbikex <csatgabor@gmail.com>
2025-07-04 20:49:01 +0200

    Change: class PasswordGenerate created with new oop style

--- Web-Development/Misc/Project-4/V2/script.js
+++ Web-Development/Misc/Project-4/V2/script.js
@@ -10,67 +10,6 @@ const pMessage = document.querySelector(".display.msg");
 const responseMsg = {
   genPw: "Here is your password:",
 };
-console.log(btnGenerate);
-console.log(btnReset);
-console.log(inputLength);
-console.log(pDisplay);
-console.log(pMessage);
-
-function generatePassword(pwLong = 16) {
-  let newPassword;
-  let passwordTemplate;
-
-  // prettier-ignore
-  const charList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  // prettier-ignore
-  const symList = [" ","!","”","#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~",];
-
-  let numOpt = 0;
-
-  pwLong % 3 === 0 ? (numOpt = pwLong / 3) : (numOpt = Math.floor(pwLong / 3));
-  console.log(numOpt);
-
-  const charLength = numOpt;
-  const numLength = numOpt;
-  const symLength = pwLong - numOpt * 2;
-
-  console.log(charLength, numLength, symLength);
-
-  passwordTemplate = loopString(charList, charLength);
-  passwordTemplate += loopString(numList, numLength);
-  passwordTemplate += loopString(symList, symLength);
-
-  console.log(`Template string: ${passwordTemplate}`);
-
-  newPassword = passwordTemplate
-    .split("")
-    .sort(() => 0.5 - Math.random())
-    .join("");
-
-  console.log(newPassword);
-  return newPassword;
-}
-
-function loopString(arr, num) {
-  let tmpPart = "";
-  for (let i = 0; i < num; i++) {
-    const randomIndex = Math.trunc(Math.random() * arr.length);
-    tmpPart += arr[randomIndex];
-  }
-  console.log(`Inner function log: ${tmpPart}`);
-  return tmpPart;
-}
-
-btnGenerate.addEventListener("click", () => {
-  console.log("Test");
-  console.log(inputLength.value);
-  let pwDisplay = generatePassword(inputLength.value || undefined);
-  pMessage.textContent = responseMsg.genPw;
-  pDisplay.textContent = pwDisplay;
-  btnCopy.appendChild(btnCopyText);
-  pDisplay.appendChild(btnCopy);
-});
 
 btnCopy.addEventListener("click", () => {
   console.log("Copy");
@@ -85,3 +24,78 @@ btnReset.addEventListener("click", () => {
   pDisplay.textContent = "";
   inputLength.value = "";
 });
+
+class PasswordGenerate {
+  //pwLong = 16;
+  constructor(pwLong) {
+    this.pwLong = pwLong;
+
+    this._loopString();
+
+    btnGenerate.addEventListener("click", () => {
+      console.log("Test");
+      this._generatePassword();
+      /*
+      console.log("Test");
+      console.log(inputLength.value);
+      let pwDisplay = _generatePassword(inputLength.value || undefined).bind(
+        this
+      );
+      pMessage.textContent = responseMsg.genPw;
+      pDisplay.textContent = pwDisplay;
+      btnCopy.appendChild(btnCopyText);
+      pDisplay.appendChild(btnCopy);
+      */
+    });
+  }
+
+  _loopString(arr, num) {
+    let tmpPart = "";
+    for (let i = 0; i < num; i++) {
+      const randomIndex = Math.trunc(Math.random() * arr.length);
+      tmpPart += arr[randomIndex];
+    }
+    console.log(`Inner function log: ${tmpPart}`);
+    return tmpPart;
+  }
+
+  _generatePassword() {
+    let newPassword;
+    let passwordTemplate;
+
+    // prettier-ignore
+    const charList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    const numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    // prettier-ignore
+    const symList = [" ","!","”","#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~",];
+
+    let numOpt = 0;
+
+    this.pwLong % 3 === 0
+      ? (numOpt = this.pwLong / 3)
+      : (numOpt = Math.floor(this.pwLong / 3));
+    console.log(numOpt);
+
+    const charLength = numOpt;
+    const numLength = numOpt;
+    const symLength = this.pwLong - numOpt * 2;
+
+    console.log(charLength, numLength, symLength);
+
+    passwordTemplate = this._loopString(charList, charLength);
+    passwordTemplate += this._loopString(numList, numLength);
+    passwordTemplate += this._loopString(symList, symLength);
+
+    console.log(`Template string: ${passwordTemplate}`);
+
+    newPassword = passwordTemplate
+      .split("")
+      .sort(() => 0.5 - Math.random())
+      .join("");
+
+    console.log(newPassword);
+    return newPassword;
+  }
+}
+
+const appPasswordGenerator = new PasswordGenerate(10);
