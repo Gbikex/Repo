ec30f1acd4e3bec19777eba9379c24239942ccda
Gbikex <csatgabor@gmail.com>
2025-11-11 19:12:21 +0100

    Add: basic password chunk logic

--- Web-Development/Misc/Project-4/V3/src/js/views/genPasswordView.js
+++ Web-Development/Misc/Project-4/V3/src/js/views/genPasswordView.js
@@ -21,26 +21,38 @@ class GeneratePassword {
     console.log(pLength);
     console.log(this.#characters, this.#characters.length);
 
-    /*
-    const randomIndex = Math.floor(Math.random() * this.characters.length) + 1;
-    console.log(randomIndex);
-    */
-
-    for (let i = 0; i <= pLength - 1; i++)
-      this.newPassword +=
-        this.#characters[this.generateRandomIndex(this.#characters)];
-    console.log(this.newPassword);
+    //Generating chunks for the password
+    this.generateChunk(pLength, this.#characters);
+    this.generateChunk(pLength, this.#numbers);
+    this.generateChunk(pLength, this.#symbols);
   }
 
   /**
    * Returns random index from the three arrays (character,numbers,symbols) to create the random new set of characters for the password
-   * @param {*} pInput can be character,number or symbol
+   * @param {*} pInput can be character,number or symbol private elements of the class
    */
   generateRandomIndex(pInput) {
-    return Math.floor(Math.random() * pInput.length) + 1;
+    console.log(Math.floor(Math.random() * pInput.length));
+    return Math.floor(Math.random() * pInput.length);
   }
 
-  generateChunk() {}
+  /**
+   * Returns the newly generated chunk for the build of the password
+   * @param {Number} pLength Length of the element
+   * @param {Private field} pElement The chunk that has to be processed
+   */
+  generateChunk(pLength, pElement) {
+    for (let i = 0; i <= pLength - 1; i++)
+      console.log(
+        (this.newPassword += pElement[this.generateRandomIndex(pElement)])
+      );
+    this.newPassword += pElement[this.generateRandomIndex(pElement)];
+    return this.newPassword;
+  }
+
+  printVariables() {
+    console.log(this.newPassword);
+  }
 }
 
 export default new GeneratePassword();
