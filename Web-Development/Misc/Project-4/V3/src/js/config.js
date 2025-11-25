ece3b02e5702f705ce6a3a8363ecb5f723040b89
Gbikex <csatgabor@gmail.com>
2025-11-25 21:29:07 +0100

    Add: config file for the pw generation

--- Web-Development/Misc/Project-4/V3/src/js/config.js
+++ Web-Development/Misc/Project-4/V3/src/js/config.js
@@ -1,3 +1,8 @@
-export const CHARACTERS = ["a", "b", "c"];
-export const NUMBERS = [1, 2, 3];
-export const SYMBOLS = ["§", "'", "+"];
+// prettier-ignore
+export const CHARACTERS = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+];
+// prettier-ignore
+export const NUMBERS = [1,2,3,4,5,6,7,8,9,0];
+// prettier-ignore
+export const SYMBOLS = ["§","@","+","!","%","/","=","(",")","?",".",":","_","-",
+];
