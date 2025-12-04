11c31ac0feea4a3dd9fe5d2ca23d3d61c82e39de
Gbikex <csatgabor@gmail.com>
2025-12-04 21:08:01 +0100

    Add: new global variables

--- Web-Development/Misc/Project-4/V3/src/js/config.js
+++ Web-Development/Misc/Project-4/V3/src/js/config.js
@@ -1,8 +1,11 @@
 // prettier-ignore
 export const CHARACTERS = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 ];
-// prettier-ignore
-export const NUMBERS = [1,2,3,4,5,6,7,8,9,0];
+export const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // prettier-ignore
 export const SYMBOLS = ["§","@","+","!","%","/","=","(",")","?",".",":","_","-",
 ];
+
+export const MINLENGTH = 9;
+export const GENERAL_PW_LENGTH = 10;
+export const BASE_SYMBOL_LENGTH = 4;
