bdd2ae17690f9d7d2c34433b3feb4125dd3002a6
Gbikex <csatgabor@gmail.com>
2025-12-09 21:27:37 +0100

    Add: timeout function

--- Web-Development/Misc/Project-4/V3/src/js/helper.js
+++ Web-Development/Misc/Project-4/V3/src/js/helper.js
@@ -1,3 +1,5 @@
+import { TIMEOUT_IN_SEC } from "./config.js";
+
 /**
  * Returns random index from the three arrays (character,numbers,symbols) to create the random new set of characters for the password
  * @param {Array} pInput can be character,number or symbol private elements of the class
@@ -5,4 +7,13 @@
 export const generateRandomIndex = function (pInput) {
   console.log(Math.floor(Math.random() * pInput.length));
   return Math.floor(Math.random() * pInput.length);
+};
+
+export const logTimeout = function (p) {
+  console.log(p);
+  setTimeout(() => {
+    console.log("!! - Test timeout print - !!");
+    console.log(p);
+    p.innerHTML = "";
+  }, TIMEOUT_IN_SEC);
 };
