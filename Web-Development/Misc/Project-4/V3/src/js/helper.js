2fe02ea21049711d3399eba1e0e9a931fbc29069
Gbikex <csatgabor@gmail.com>
2025-12-14 10:37:06 +0100

    Change: previously commited timeout id clear rewordked new function added to handle the previously started runs

--- Web-Development/Misc/Project-4/V3/src/js/helper.js
+++ Web-Development/Misc/Project-4/V3/src/js/helper.js
@@ -1,3 +1,4 @@
+let GlobalTimeOutID = "";
 /**
  * Standard function to create random indexes for further processes
  * @param {Array} pInput
@@ -13,9 +14,19 @@ export const generateRandomIndex = function (pInput) {
  * @param {Number} pDelay - Seconds for the delay
  */
 export const resetElement = function (pElement, pDelay) {
+  resetTimeoutID();
+
   const timeOutID = setTimeout(() => {
     pElement.innerHTML = "";
   }, pDelay);
 
-  clearTimeout(timeOutID);
+  console.log("resEl log");
+  GlobalTimeOutID = timeOutID;
+  console.log(GlobalTimeOutID);
+};
+/**
+ * Clears previously started timeouts
+ */
+const resetTimeoutID = function () {
+  clearTimeout(GlobalTimeOutID);
 };
