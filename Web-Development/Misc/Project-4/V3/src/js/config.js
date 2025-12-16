578721c4fe5c923ef7ef8c8405abc8c489cd7619
Gbikex <csatgabor@gmail.com>
2025-12-16 18:46:42 +0100

    Add: new global variables to adjust the sass variables

--- Web-Development/Misc/Project-4/V3/src/js/config.js
+++ Web-Development/Misc/Project-4/V3/src/js/config.js
@@ -5,9 +5,14 @@ export const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // prettier-ignore
 export const SYMBOLS = ["§","@","+","!","%","/","=","(",")","?",".",":","_","-",
 ];
-
+//Password related global variables
 export const MINLENGTH = 9;
 export const MAX_LENGTH = 35;
 export const GENERAL_PW_LENGTH = 10;
 export const BASE_SYMBOL_LENGTH = 4;
 export const LOG_ELEMENT_RESET = 5000;
+
+//Changing SASS related global variables
+export const MODAL_DISPLAY_VAL_ON = "block";
+export const MODAL_DISPLAY_VAL_OFF = "none";
+export const MODAL_DISPLAY_VARIABLE = "--displayType";
