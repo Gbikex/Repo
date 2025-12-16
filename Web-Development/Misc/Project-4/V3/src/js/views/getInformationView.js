f2f54f7ca4152a90ed4c2f5fab7737a9cce70746
Gbikex <csatgabor@gmail.com>
2025-12-16 18:46:15 +0100

    Add: handlers to open or close modal with new changeproperty function

--- Web-Development/Misc/Project-4/V3/src/js/views/getInformationView.js
+++ Web-Development/Misc/Project-4/V3/src/js/views/getInformationView.js
@@ -1,17 +1,41 @@
 class GetInformationView {
   _infoEL = document.querySelector(".btn_info");
   _infoDiv = document.querySelector(".app_info_modal");
+  _infoClose = document.querySelector(".app_info_close");
 
   test() {
     console.log("clicked");
   }
 
-  addHandlerInfo(handler) {
+  /**
+   * Changes the variables in the SASS files used for opening and closing the modal dialog by adding or removing display -> block,none
+   * @param {String} pElement --
+   * @param {String} pProperty
+   */
+  changeElement(pVariable, pProperty) {
+    console.log("test");
+    document.documentElement.style.setProperty(pVariable, pProperty);
+  }
+
+  /**
+   * Open information dialog window, when the ? button clicked css property changed via adjusting a variable.
+   * @param {*} handler
+   */
+  addHandlerInfoOpen(handler) {
     console.log(this._infoDiv);
     this._infoEL.addEventListener("click", function (e) {
       e.preventDefault();
-      //Change the property of SASS variable
-      document.documentElement.style.setProperty("--displayType", "block");
+      handler();
+    });
+  }
+
+  /**
+   * Closes the dialog window via adding back the block property to the SASS variable
+   * @param {*} handler
+   */
+  addHandlerInfoClose(handler) {
+    this._infoClose.addEventListener("click", function (e) {
+      e.preventDefault();
       handler();
     });
   }
