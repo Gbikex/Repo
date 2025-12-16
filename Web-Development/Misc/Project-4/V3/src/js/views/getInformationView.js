a4330c15e5ffa74559b9d5d525777069bb96859b
Gbikex <csatgabor@gmail.com>
2025-12-16 18:14:31 +0100

    Add: change sass variable

--- Web-Development/Misc/Project-4/V3/src/js/views/getInformationView.js
+++ Web-Development/Misc/Project-4/V3/src/js/views/getInformationView.js
@@ -1,14 +1,17 @@
 class GetInformationView {
-  _infoEL = document.querySelector(".app_info_p");
+  _infoEL = document.querySelector(".btn_info");
+  _infoDiv = document.querySelector(".app_info_modal");
 
   test() {
     console.log("clicked");
   }
 
   addHandlerInfo(handler) {
+    console.log(this._infoDiv);
     this._infoEL.addEventListener("click", function (e) {
       e.preventDefault();
-
+      //Change the property of SASS variable
+      document.documentElement.style.setProperty("--displayType", "block");
       handler();
     });
   }
