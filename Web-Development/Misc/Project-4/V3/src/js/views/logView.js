0f3e58878a8fd215dee1ff492148a2f7c8f4b368
Gbikex <csatgabor@gmail.com>
2025-12-17 21:50:58 +0100

    Add: general error rendering method

--- Web-Development/Misc/Project-4/V3/src/js/views/logView.js
+++ Web-Development/Misc/Project-4/V3/src/js/views/logView.js
@@ -24,6 +24,18 @@ class LogView {
   }
 
   /**
+   * Dynamic error render procedure for the possible input limits to generate a password
+   * @param {String} pInputElement -- DOM Element that is required for the check
+   * @param {Number} pLimit -- Limit in number to check if it is failed or not
+   * @param {String} pErrorMsg -- Error message that is passed to renderErrorElement method to show it in the DOM
+   * @returns
+   */
+  renderError(pInputElement, pLimit, pErrorMsg) {
+    if (pInputElement.value < pLimit && pInputElement.value)
+      return this.renderErrorElement(pErrorMsg);
+  }
+
+  /**
    * Clears the dedicated log responses for certain processes
    */
   clearLog() {
