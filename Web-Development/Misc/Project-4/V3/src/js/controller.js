b1f4ff69d815e06edf878e209176606ba4ae9b40
Gbikex <csatgabor@gmail.com>
2025-12-17 22:06:47 +0100

    Change: error handling added to controller

--- Web-Development/Misc/Project-4/V3/src/js/controller.js
+++ Web-Development/Misc/Project-4/V3/src/js/controller.js
@@ -3,16 +3,32 @@ import {
   BTN_COPY_VARIABLE,
   DISPLAY_VAL_ON,
   DISPLAY_VAL_OFF,
+  MINLENGTH,
+  MAX_LENGTH,
 } from "./config.js";
+import { state } from "./model.js";
 import { changeElement } from "./helper.js";
 import generatePasswordView from "./views/genPasswordView.js";
 import copyPasswordView from "./views/copyPasswordView.js";
 import getInformationView from "./views/getInformationView.js";
+import logView from "./views/logView.js";
 
 console.log("Controller");
 
 // Generates password
 const controlPasswordGeneration = function () {
+  // 1) Checking if the minimum limit met
+  if (
+    generatePasswordView.inputElement.value < MINLENGTH &&
+    generatePasswordView.inputElement.value
+  )
+    return logView.renderErrorElement(state.responses.pwMinLengthMsg);
+
+  // 2) Checking if the max limit not over shot
+  if (generatePasswordView.inputElement.value > MAX_LENGTH)
+    return logView.renderErrorElement(state.responses.pwMaxLengthMSG);
+
+  // 3) If all pre requirements met pw is generated
   generatePasswordView.renderPassword();
   changeElement(BTN_COPY_VARIABLE, DISPLAY_VAL_ON);
 };
@@ -23,7 +39,7 @@ const controlPasswordCopy = function () {
 // Reset interface
 const controlReset = function () {
   generatePasswordView.clear();
-  generatePasswordView._inputElement.value = "";
+  generatePasswordView.inputElement.value = "";
   changeElement(BTN_COPY_VARIABLE, DISPLAY_VAL_OFF);
 };
 // Open modal dialog
