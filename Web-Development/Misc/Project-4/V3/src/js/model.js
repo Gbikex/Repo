b047a13abdef5eb9f593a334c8f1911dbf3f0b4f
Gbikex <csatgabor@gmail.com>
2025-12-27 22:15:03 +0100

    Refactoring: not used object element removed

--- Web-Development/Misc/Project-4/V3/src/js/model.js
+++ Web-Development/Misc/Project-4/V3/src/js/model.js
@@ -5,9 +5,6 @@ export const state = {
     pwMaxLengthMSG: "The generated password cannot be more then 40 characters!",
     pwGeneration: "Here is your generated password",
   },
-  errorMsg: "",
-  incorrectPwLength: "The generated password must be minimum 9 characters!",
-  successMsg: "Here is your generated password",
   log: {
     success: "All good :)",
     fail: "Oh, there is some problem",
