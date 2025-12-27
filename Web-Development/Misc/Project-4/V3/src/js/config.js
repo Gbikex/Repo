040120c5a6c187871c426ed010329aa2c364615f
Gbikex <csatgabor@gmail.com>
2025-12-27 21:53:02 +0100

    Refactoring: minimal comment changes

--- Web-Development/Misc/Project-4/V3/src/js/config.js
+++ Web-Development/Misc/Project-4/V3/src/js/config.js
@@ -1,3 +1,4 @@
+//Password generation variables
 // prettier-ignore
 export const CHARACTERS = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 ];
@@ -5,6 +6,7 @@ export const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // prettier-ignore
 export const SYMBOLS = ["§","@","+","!","%","/","=","(",")","?",".",":","_","-",
 ];
+
 //Password related global variables
 export const MINLENGTH = 9;
 export const MAX_LENGTH = 35;
@@ -13,6 +15,7 @@ export const BASE_SYMBOL_LENGTH = 4;
 export const LOG_ELEMENT_RESET = 5000;
 
 //Changing SASS related global variables
-export const MODAL_DISPLAY_VAL_ON = "block";
-export const MODAL_DISPLAY_VAL_OFF = "none";
+export const DISPLAY_VAL_ON = "block";
+export const DISPLAY_VAL_OFF = "none";
 export const MODAL_DISPLAY_VARIABLE = "--displayTypeDiv";
+export const BTN_COPY_VARIABLE = "--displayTypeBtn";
