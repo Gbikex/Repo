c03822966ee84f5d49189a8baad0cd5c171ce447
Gbikex <csatgabor@gmail.com>
2026-01-19 20:09:44 +0100

    Add: Steps component

--- Web-Development/Playground/React/DateCounter/src/index.js
+++ Web-Development/Playground/React/DateCounter/src/index.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function Test() {
-  return <p>Test</p>;
+function Counter() {
+  const [step, setStep] = useState(0);
+
+  function HandleNextStep() {
+    setStep((step) => step + 1);
+  }
+  function HandlePreviousStep() {
+    if (step > 0) setStep((step) => step - 1);
+  }
+
+  return (
+    <div>
+      <div className="div div_step">
+        <button className="btn btn_minus" onClick={HandlePreviousStep}>
+          -
+        </button>
+        <p>Steps: {step}</p>
+
+        <button className="btn btn_add" onClick={HandleNextStep}>
+          +
+        </button>
+      </div>
+      <div className="count"></div>
+    </div>
+  );
 }
 
 export default function App() {
   return (
     <div>
-      <Test />
+      <Counter />
     </div>
   );
 }
@@ -18,5 +41,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
