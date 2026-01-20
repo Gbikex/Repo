add5cf23505b2684999d94a4573ca29ece93967f
Gbikex <csatgabor@gmail.com>
2026-01-20 21:06:46 +0100

    Add: event delegation handler, not perfect rework needed, this is just a skeleton

--- Web-Development/Misc/Project-6/V2/src/js/view/pickUserView.js
+++ Web-Development/Misc/Project-6/V2/src/js/view/pickUserView.js
@@ -1,12 +1,39 @@
+import { MOVES } from "../config";
+
 class PickUser {
   _rock = document.querySelector(".pick-rock");
   _paper = document.querySelector(".pick-paper");
   _scissor = document.querySelector(".pick-scissor");
 
-  handlerUserPick(handler) {
-    this._rock.addEventListener("click", function (e) {
-      e.preventDefault();
-      handler();
+  _container = document.querySelector(".container");
+
+  userMove = null;
+
+  handlerUserPick() {
+    this._container.addEventListener("click", function (e) {
+      let userPick = "";
+
+      const btn = e.target.closest(".btn");
+
+      // Click was outside a button
+      if (!btn) return;
+
+      // Determine which button was clicked
+      if (btn.classList.contains("pick-rock")) {
+        //console.log(MOVES[0]);
+        handler((this.userMove = MOVES[0]));
+      }
+
+      if (btn.classList.contains("pick-paper")) {
+        //console.log(MOVES[1]);
+        handler(MOVES[1]);
+      }
+
+      if (btn.classList.contains("pick-scissor")) {
+        //console.log(MOVES[2]);
+        handler(MOVES[2]);
+      }
+      //console.log(userPick);
     });
   }
 
