377c8293c7480dd3322df0ca49e982187a5b5b44
Gbikex <csatgabor@gmail.com>
2026-01-21 20:20:51 +0100

    Add: computer move method finalized

--- Web-Development/Misc/Project-6/V2/src/js/view/pickComputerView.js
+++ Web-Development/Misc/Project-6/V2/src/js/view/pickComputerView.js
@@ -1,10 +1,24 @@
 import { genRandomNumber } from "../helper";
-import { GEN_NUMBER_INDEX } from "../config";
+import { GEN_NUMBER_INDEX, MOVES } from "../config";
 
 class PickComputer {
-  testPrint() {
-    console.log("Hello from the Pick Computer view");
-    console.log(genRandomNumber(GEN_NUMBER_INDEX));
+  /**
+   * Randomly generates a computer move using a helper function witch generates a random number between 1 and 3.
+   * @returns -> Computer move
+   */
+  computerPick() {
+    const index = genRandomNumber(GEN_NUMBER_INDEX);
+
+    switch (index) {
+      case 1:
+        return MOVES[0];
+      case 2:
+        return MOVES[1];
+      case 3:
+        return MOVES[2];
+      default:
+        return "Invalid move, please try again!";
+    }
   }
 }
 
