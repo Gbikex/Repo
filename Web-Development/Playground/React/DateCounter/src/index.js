02a0fdea7ec0c2b436509e9349a325e09cbc34f6
Gbikex <csatgabor@gmail.com>
2026-01-26 20:04:48 +0100

    Add: new elements a logic to handle range and and input

--- Web-Development/Playground/React/DateCounter/src/index.js
+++ Web-Development/Playground/React/DateCounter/src/index.js
@@ -6,14 +6,6 @@ function Counter() {
   const [step, setStep] = useState(0);
   const [count, setCount] = useState(0);
 
-  //Steps handler
-  function HandleNextStep() {
-    setStep((step) => step + 1);
-  }
-  function HandlePreviousStep() {
-    setStep((step) => step - 1);
-  }
-
   //Count handler
   function HandleNextCount() {
     setCount((count) => count + step);
@@ -21,6 +13,13 @@ function Counter() {
 
   function HandlePreviousCount() {
     setCount((count) => count - step);
+  }
+
+  //Handle reset
+
+  function handleReset() {
+    setStep(0);
+    setCount(0);
   }
 
   const dateToday = function (pCount) {
@@ -33,20 +32,25 @@ function Counter() {
   return (
     <div>
       <div className="div div_step">
-        <button className="btn btn_minus" onClick={HandlePreviousStep}>
-          -
-        </button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
         <p>Steps: {step}</p>
-
-        <button className="btn btn_add" onClick={HandleNextStep}>
-          +
-        </button>
       </div>
       <div className="div div_count">
         <button className="btn btn_minus" onClick={HandlePreviousCount}>
           -
         </button>
-        <p>Count: {count}</p>
+        <input
+          type="text"
+          defaultValue="0"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
         <button className="btn btn_add" onClick={HandleNextCount}>
           +
         </button>
@@ -54,13 +58,19 @@ function Counter() {
       <div className="div set_date">
         <p>
           {count === 0
-            ? "Today is"
+            ? "Today is "
             : count > 0
               ? `${count} days from today is `
-              : `${count} days ago was`}{" "}
+              : `${count} days ago was `}
           {dateToday(count).toLocaleDateString()}
         </p>
       </div>
+      <button
+        style={step > 0 ? { display: "block" } : { display: "none" }}
+        onClick={handleReset}
+      >
+        Reset
+      </button>
     </div>
   );
 }
