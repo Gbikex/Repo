369c3f0b5ccc30fb4fbe561ff2ea5322130513ef
Gbikex <csatgabor@gmail.com>
2026-01-31 10:40:42 +0100

    Add: skeleton of components with base styling wip

--- Web-Development/Playground/React/TaskManager/src/App.js
+++ Web-Development/Playground/React/TaskManager/src/App.js
@@ -1,7 +1,48 @@
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <p>Welcome from App.js</p>
+      <Header />
+      <Forms />
+      <Tasks />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Hello from Header</h1>
+    </div>
+  );
+}
+
+function Forms() {
+  return (
+    <div className="forms">
+      <p>Hello from Form</p>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
+}
+
+function Tasks() {
+  return (
+    <div>
+      <p>Hello from Tasks</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <p>Hello from Footer</p>
     </div>
   );
 }
