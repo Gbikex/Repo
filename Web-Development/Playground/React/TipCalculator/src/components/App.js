43169e0e835237c664a47e95a2c69ee0a8418ab5
Gbikex <csatgabor@gmail.com>
2026-02-01 14:13:37 +0100

    Add:state for events

--- Web-Development/Playground/React/TipCalculator/src/components/App.js
+++ Web-Development/Playground/React/TipCalculator/src/components/App.js
@@ -10,6 +10,8 @@ export default function App() {
 
 function HandleBill() {
   const [bill, setBill] = useState();
+  const [mySatisfaction, setMySatisfaction] = useState();
+  const [friendSatisfaction, setFriendSatisfaction] = useState();
 
   return (
     <div>
@@ -19,12 +21,14 @@ function HandleBill() {
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
       />
-      <Button>{<span>Reset</span>}</Button>
       <ServiceSatisfaction>
         {
           <div>
             <p>How did you like the service:</p>
-            <select>
+            <select
+              value={mySatisfaction}
+              onChange={(e) => setMySatisfaction(Number(e.target.value))}
+            >
               <option value={0} key={1}>
                 Dissatisfied (0%)
               </option>
@@ -41,6 +45,36 @@ function HandleBill() {
           </div>
         }
       </ServiceSatisfaction>
+      <ServiceSatisfaction>
+        {
+          <div>
+            <p>How did your friend like the service:</p>
+            <select
+              value={friendSatisfaction}
+              onChange={(e) => setFriendSatisfaction(Number(e.target.value))}
+            >
+              <option value={0} key={1}>
+                Dissatisfied (0%)
+              </option>
+              <option value={5} key={2}>
+                It was okay (5%)
+              </option>
+              <option value={10} key={3}>
+                It was good (10%)
+              </option>
+              <option value={20} key={4}>
+                Absolutely amazing (20%)
+              </option>
+            </select>
+          </div>
+        }
+      </ServiceSatisfaction>
+      <Button>{<span>Reset</span>}</Button>
+      <div>
+        <p>
+          You pay $ {bill} {mySatisfaction} {friendSatisfaction}
+        </p>
+      </div>
     </div>
   );
 }
