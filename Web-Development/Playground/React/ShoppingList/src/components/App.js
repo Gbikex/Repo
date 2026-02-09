58ed8e2f55eb8be47c5fd1d64bf820c277fa3893
Gbikex <csatgabor@gmail.com>
2026-02-09 20:14:00 +0100

    Add: shopping list form and state

--- Web-Development/Playground/React/ShoppingList/src/components/App.js
+++ Web-Development/Playground/React/ShoppingList/src/components/App.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  function handleShoppingList(item) {
+    setShoppingList((items) => [...items, item]);
+  }
+
   return (
     <div>
       <p>Test</p>
       <Header />
-      <ShoppingListForm />
+      <ShoppingListForm onAddShoppingItem={handleShoppingList} />
       <ShoppingList />
       <Footer />
     </div>
@@ -20,12 +26,50 @@ function Header() {
   );
 }
 
-function ShoppingListForm() {
-  const [shoppingList, setShoppingList] = useState("");
+function ShoppingListForm({ onAddShoppingItem }) {
+  const [product, setProduct] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const shoppingList = {
+      product,
+      quantity,
+    };
+
+    console.log(shoppingList);
+
+    onAddShoppingItem(shoppingList);
+  }
 
   return (
     <div className="shopping-list-form">
       <p>This is the shopping list form area</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Item to the list"
+          value={product}
+          onChange={(e) => {
+            setProduct(e.target.value);
+          }}
+        />
+        <select
+          className="select"
+          value={quantity}
+          onChange={(e) => {
+            setQuantity(+e.target.value);
+          }}
+        >
+          {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <button>Add to the list</button>
+      </form>
     </div>
   );
 }
