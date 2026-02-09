18cd5814b1f5c231a64ddd67cce4a510f2578993
Gbikex <csatgabor@gmail.com>
2026-02-09 19:34:53 +0100

    Add: base components

--- Web-Development/Playground/React/ShoppingList/src/components/App.js
+++ Web-Development/Playground/React/ShoppingList/src/components/App.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div>
       <p>Test</p>
+      <Header />
+      <ShoppingListForm />
+      <ShoppingList />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <p> 🍋 Shopping list 🥩</p>
+    </div>
+  );
+}
+
+function ShoppingListForm() {
+  const [shoppingList, setShoppingList] = useState("");
+
+  return (
+    <div className="shopping-list-form">
+      <p>This is the shopping list form area</p>
+    </div>
+  );
+}
+
+function ShoppingList() {
+  return (
+    <div className="shopping-list">
+      <p>Render shopping list</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>This is the footer</p>
     </div>
   );
 }
