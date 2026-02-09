79241fc1ec034aaa01f0c8ebd228cc79768fdeae
Gbikex <csatgabor@gmail.com>
2026-02-09 19:23:57 +0100

    Add: skeleton of project

---
+++ Web-Development/Playground/React/ShoppingList/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App.js";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
