eb536c8871614215ad97bef820ec76e187c9ed31
Gbikex <csatgabor@gmail.com>
2026-02-22 17:55:03 +0100

    Add: currency converter projectr

---
+++ Web-Development/Playground/React/CurrencyConverter/src/App.js
@@ -0,0 +1,72 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [inputAmount, setInputAmount] = useState("");
+  const [convertFrom, setConvertFrom] = useState("USD");
+  const [convertTo, setConvertTo] = useState("EUR");
+  const [outPut, setOutPut] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      if (inputAmount === "") return;
+
+      if (convertFrom === convertTo)
+        throw new Error(
+          "The currencies to exchange the entered volume must be different",
+        );
+
+      try {
+        async function useConverter() {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${inputAmount}&from=${convertFrom}&to=${convertTo}`,
+          );
+
+          const data = await res.json();
+          console.log(data);
+          const { rates } = data;
+          const rate = rates[convertTo];
+          console.log(rate);
+          setOutPut(rate);
+        }
+        useConverter();
+      } catch (error) {
+        //console.error(error);
+        setError(error);
+      } finally {
+        setError("");
+      }
+    },
+    [inputAmount, convertFrom, convertTo],
+  );
+
+  return (
+    <div>
+      <input
+        value={inputAmount}
+        onChange={(e) => {
+          setInputAmount(Number(e.target.value));
+        }}
+      />
+      <select
+        value={convertFrom}
+        onChange={(e) => setConvertFrom(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={convertTo} onChange={(e) => setConvertTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT: {outPut}</p>
+      {!error && <p>{error}</p>}
+    </div>
+  );
+}
