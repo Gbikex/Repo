b79d1fa1f87c69079f719b35551a4263dad1e658
Gbikex <csatgabor@gmail.com>
2026-04-03 13:24:44 +0200

    Add: general button component

--- Web-Development/Playground/React/SimpleBanking/V2/src/pages/Homepage.jsx
+++ Web-Development/Playground/React/SimpleBanking/V2/src/pages/Homepage.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router";
+
+import styles from "./Homepage.module.css";
+
+import ToAccount from "../components/ToAccount";
+
 function Homepage() {
   return (
-    <div>
-      <section>
-        <h1>Welcome to the Simple Banking</h1>
-      </section>
+    <div className={styles.app}>
+      <h1>Welcome to the Simple Banking</h1>
+      <div>
+        <Link to="/account">
+          <ToAccount />
+        </Link>
+      </div>
     </div>
   );
 }
