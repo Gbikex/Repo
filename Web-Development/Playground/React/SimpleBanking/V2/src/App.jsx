459dd1c40d612519eb81b7952bb8f950df9a5d40
Gbikex <csatgabor@gmail.com>
2026-04-03 11:11:32 +0200

    Add: base routing setup

--- Web-Development/Playground/React/SimpleBanking/V2/src/App.jsx
+++ Web-Development/Playground/React/SimpleBanking/V2/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import Account from "./pages/Account";
 
 function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="account" element={<Account />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ Web-Development/Playground/React/SimpleBanking/V2/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+function Homepage() {
+  return (
+    <div>
+      <section>
+        <h1>Welcome to the Simple Banking</h1>
+      </section>
+    </div>
+  );
+}
+
+export default Homepage;
