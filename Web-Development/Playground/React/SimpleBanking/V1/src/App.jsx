26c9aee08669051bb0915e58c821bd679c076cb1
Gbikex <csatgabor@gmail.com>
2026-04-03 10:06:51 +0200

    Change: move project to new versioned folder

---
+++ Web-Development/Playground/React/SimpleBanking/V1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import Banking from "./pages/Banking";
+
+function App() {
+  return (
+    <div>
+      <Banking />
+    </div>
+  );
+}
+
+export default App;
