bec90065e500ff446c1643eaf8dfc340b56533fc
Gbikex <csatgabor@gmail.com>
2026-04-03 11:36:51 +0200

    Add: components folder with account comp

---
+++ Web-Development/Playground/React/SimpleBanking/V2/src/components/ToAccount.jsx
@@ -0,0 +1,11 @@
+import styles from "./ToAccount.module.css";
+
+function ToAccount() {
+  return (
+    <div className={styles.container}>
+      <h2>Go to your account</h2>
+    </div>
+  );
+}
+
+export default ToAccount;
