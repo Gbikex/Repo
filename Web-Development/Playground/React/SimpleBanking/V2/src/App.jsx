dc2021b5611e95c9078e1e17e66a542771b75153
Gbikex <csatgabor@gmail.com>
2026-04-04 18:28:12 +0200

    Add: Account provider to the layout

--- Web-Development/Playground/React/SimpleBanking/V2/src/App.jsx
+++ Web-Development/Playground/React/SimpleBanking/V2/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Account from "./pages/Account";
 
+import { AccountProvider } from "./contexts/AccountContexts";
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="account" element={<Account />} />
-        </Routes>
-      </BrowserRouter>
+      <AccountProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="account" element={<Account />} />
+          </Routes>
+        </BrowserRouter>
+      </AccountProvider>
     </div>
   );
 }
