13ac5aa675219ee11d60c25b7b2ceb136aee4fc9
Gbikex <csatgabor@gmail.com>
2026-04-05 22:30:58 +0200

    Add: error handles to limit user inputs for withdraw and loan request with dedicated variables and error msg

--- Web-Development/Playground/React/SimpleBanking/V2/src/contexts/AccountContexts.jsx
+++ Web-Development/Playground/React/SimpleBanking/V2/src/contexts/AccountContexts.jsx
@@ -8,19 +8,37 @@ const initialState = {
   errorMsg: "",
   balance: 0,
   loan: 0,
+  loanReq: 5000,
   depositInput: 0,
   withdrawInput: 0,
   maxWithdraw: 5000,
   requestLoanInput: 0,
   maxLoan: 10000,
+  maxAvailableLoan: 15000,
   payLoanInput: 0,
 };
 
-const errorMessages = {
-  withdrawError: `The withdraw amount is more then the allowed limit as of ${initialState.maxWithdraw}`,
-};
-
 function reducer(state, action) {
+  // Handle state logics
+  // 1) Withdraw constrains
+  const withdrawMaxLimit = state.withdrawInput > state.maxWithdraw;
+  const withdrawBalanceLimit = state.withdrawInput > state.balance;
+  // 2) Loan constrains on requesting
+  const loanLimit = state.requestLoanInput > state.maxLoan;
+  const loanRequirement = state.balance < state.loanReq;
+  const loanDemandMax = state.loan > state.maxAvailableLoan;
+  // 3) Loan constrains on pay back
+  const noLoan = state.loan === 0;
+  const balanceNotMet = state.balance < state.loan;
+  //Returns error messages based on state
+  const errorMessages = {
+    withdrawMaxMsg: `The withdraw amount is more then the allowed limit as of ${state.maxWithdraw}`,
+    withdrawBalanceMsg: `The requested withdraw ${state.withdrawInput} exceeds the current balance of ${state.balance}`,
+    loanMaxLimitMsg: `The requested loan exceeds the limit of  ${state.maxLoan}`,
+    loanBalanceMinMsg: `The required minimum balance (${state.loanReq}) not met`,
+    loanDemandMaxMsg: `Your loan exceeds the allowed maximum of ${state.maxAvailableLoan}`,
+  };
+
   switch (action.type) {
     case "openAccount":
       return { ...state, isOpen: true, balance: 500 };
@@ -42,11 +60,13 @@ function reducer(state, action) {
         withdrawInput: action.payLoad,
       };
     case "withdraw":
-      if (state.withdrawInput > state.maxWithdraw)
+      if (withdrawMaxLimit || withdrawBalanceLimit)
         return {
           ...state,
           isError: true,
-          errorMsg: errorMessages.withdrawError,
+          errorMsg: withdrawMaxLimit
+            ? errorMessages.withdrawMaxMsg
+            : errorMessages.withdrawBalanceMsg,
         };
 
       return {
@@ -58,10 +78,20 @@ function reducer(state, action) {
     case "inputLoanRequest":
       return { ...state, requestLoanInput: action.payLoad };
     case "loanRequest":
+      if (loanLimit || loanRequirement || loanDemandMax)
+        return {
+          ...state,
+          isError: true,
+          errorMsg: loanLimit
+            ? errorMessages.loanMaxLimitMsg
+            : loanDemandMax
+              ? errorMessages.loanDemandMaxMsg
+              : errorMessages.loanBalanceMinMsg,
+        };
       return {
         ...state,
-        loan: state.requestLoanInput,
-        balance: state.balance - state.requestLoanInput,
+        loan: state.loan + state.requestLoanInput,
+        balance: state.balance + state.requestLoanInput,
         requestLoanInput: 0,
         isError: false,
       };
