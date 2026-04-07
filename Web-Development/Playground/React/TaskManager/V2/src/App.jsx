8635d763f1d13682f6565ae3c3665311cbd51268
Gbikex <csatgabor@gmail.com>
2026-04-07 19:56:48 +0200

    Add: skeleton of routing

--- Web-Development/Playground/React/TaskManager/V2/src/App.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Homepage from "./pages/HomePage";
+import TaskCreate from "./pages/TaskCreate";
+
 function App() {
   return (
     <>
       <h1>Task Manager</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="create-task" element={<TaskCreate />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
