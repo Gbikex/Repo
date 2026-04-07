1049c2c035e67209a833a08929462e61db7bb769
Gbikex <csatgabor@gmail.com>
2026-04-07 19:56:10 +0200

    Initial commit

---
+++ Web-Development/Playground/React/TaskManager/V2/src/components/CreateNewTask.jsx
@@ -0,0 +1,11 @@
+import styles from "./CreateNewTask.module.css";
+
+function CreateNewTask() {
+  return (
+    <>
+      <p className={styles.fontColor}>Create new task</p>
+    </>
+  );
+}
+
+export default CreateNewTask;
