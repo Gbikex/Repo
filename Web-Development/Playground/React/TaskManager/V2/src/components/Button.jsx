7a02f7bb2553e33989a1a72cfc686c57edd2831b
Gbikex <csatgabor@gmail.com>
2026-04-13 21:57:32 +0200

    Add: isDisabled param

--- Web-Development/Playground/React/TaskManager/V2/src/components/Button.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/components/Button.jsx
@@ -1,10 +1,11 @@
 import styles from "./Button.module.css";
 
-function Button({ onClick, type, btnWide, children }) {
+function Button({ onClick, type, btnWide, children, isDisabled }) {
   return (
     <button
       className={`${styles.btn} ${styles[type]} ${styles[btnWide]}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
     </button>
