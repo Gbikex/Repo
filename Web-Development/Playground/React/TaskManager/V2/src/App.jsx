4547df9fcb28eec20b16bdfeace586acb26085a6
Gbikex <csatgabor@gmail.com>
2026-04-14 21:45:58 +0200

    Add: route for create project

--- Web-Development/Playground/React/TaskManager/V2/src/App.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/App.jsx
@@ -3,16 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import TaskCreate from "./pages/TaskCreate";
 
+import { TaskProvider } from "./context/Task";
+import { ProjectProvider } from "./context/Project";
+import ProjectCreate from "./pages/ProjectCreate";
+
 function App() {
   return (
     <>
       <h1>Task Manager</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="create-task" element={<TaskCreate />} />
-        </Routes>
-      </BrowserRouter>
+      <TaskProvider>
+        <ProjectProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="/create-task" element={<TaskCreate />} />
+              <Route path="/create-project" element={<ProjectCreate />} />
+            </Routes>
+          </BrowserRouter>
+        </ProjectProvider>
+      </TaskProvider>
     </>
   );
 }
