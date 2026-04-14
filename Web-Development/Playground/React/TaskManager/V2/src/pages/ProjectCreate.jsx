9a711d58966010cf6b9b5a24d608b3ab04741450
Gbikex <csatgabor@gmail.com>
2026-04-14 21:01:12 +0200

    Add: skeleton of inputs and dispatch functions to handle actions

--- Web-Development/Playground/React/TaskManager/V2/src/pages/ProjectCreate.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/pages/ProjectCreate.jsx
@@ -1,24 +1,76 @@
 import Button from "../components/Button";
 import ButtonBack from "../components/ButtonBack";
 
-import { UseProject } from "../context/Project";
+import { useProject } from "../context/Project";
 
 function ProjectCreate() {
-  const { projectName, projectDescription } = UseProject;
+  const {
+    isError,
+    projectName,
+    projectNameError,
+    projectDescription,
+    projectDescriptionError,
+    dispatch,
+  } = useProject();
+
+  function handleProjectNameInput(e) {
+    dispatch({ type: "addProjectName", payLoad: e.target.value });
+  }
+
+  function handleProjectDescriptionInput(e) {
+    dispatch({ type: "addProjectDescription", payLoad: e.target.value });
+  }
+
+  function handleResetInput() {
+    dispatch({ type: "resetInput" });
+  }
+
+  function handleCreateNewProject() {
+    const newProject = {
+      projectName,
+      projectDescription,
+    };
+
+    dispatch({ type: "addNewProject", payLoad: newProject });
+  }
 
   return (
     <>
       <p>Create New Project</p>
       <div>
         <ButtonBack />
-        <Button type="primary">Reset</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            handleResetInput();
+          }}
+        >
+          Reset
+        </Button>
       </div>
       <p>Project Name</p>
-      <input></input>
+      <input
+        value={projectName}
+        onChange={(e) => {
+          handleProjectNameInput(e);
+        }}
+      ></input>
       <p>Project Description</p>
-      <input></input>
+      <input
+        value={projectDescription}
+        onChange={(e) => {
+          handleProjectDescriptionInput(e);
+        }}
+      ></input>
       <div>
-        <Button type="create">Create</Button>
+        <Button
+          type="create"
+          onClick={() => {
+            (handleCreateNewProject(), handleResetInput());
+          }}
+        >
+          Create
+        </Button>
       </div>
     </>
   );
