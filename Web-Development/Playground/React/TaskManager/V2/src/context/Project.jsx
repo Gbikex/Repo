ed71b62a48cb74226003a42c719840e50ed0d42c
Gbikex <csatgabor@gmail.com>
2026-04-14 21:02:02 +0200

    Add: actions in reducer to handle actions

--- Web-Development/Playground/React/TaskManager/V2/src/context/Project.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/context/Project.jsx
@@ -3,29 +3,73 @@ import { createContext, useContext, useReducer } from "react";
 const ProjectContext = createContext();
 
 const initialState = {
+  isError: false,
+  projectList: [],
+  projectId: 0,
   projectName: "",
+  projectNameError: false,
+  projectDescription: "",
+  projectDescriptionError: false,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "addProject":
-      return { ...state };
+    case "addProjectName":
+      return { ...state, projectName: action.payLoad };
+    case "addProjectDescription":
+      return { ...state, projectDescription: action.payLoad };
+    case "resetInput":
+      return {
+        ...state,
+        projectName: "",
+        projectDescriptionError: false,
+        projectDescription: "",
+        projectDescriptionError: false,
+      };
+    case "addNewProject":
+      return {
+        ...state,
+        projectId: state.projectId + 1,
+        projectList: [...state.projectList, action.payLoad],
+      };
     default:
       throw new Error("Unknown action 🫥");
   }
 }
 
 function ProjectProvider({ children }) {
-  const [{ projectName, dispatch }] = useReducer(reducer, initialState);
+  const [
+    {
+      isError,
+      projectList,
+      projectId,
+      projectName,
+      projectNameError,
+      projectDescription,
+      projectDescriptionError,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   return (
-    <ProjectContext.Provider value={{ projectName, dispatch }}>
+    <ProjectContext.Provider
+      value={{
+        isError,
+        projectList,
+        projectId,
+        projectName,
+        projectNameError,
+        projectDescription,
+        projectDescriptionError,
+        dispatch,
+      }}
+    >
       {children}
     </ProjectContext.Provider>
   );
 }
 
-function UseProject() {
+function useProject() {
   const context = useContext(ProjectContext);
 
   if (context === undefined)
@@ -34,4 +78,4 @@ function UseProject() {
   return context;
 }
 
-export { ProjectProvider, UseProject };
+export { ProjectProvider, useProject };
