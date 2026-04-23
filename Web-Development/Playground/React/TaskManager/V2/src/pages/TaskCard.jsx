8f86c003b8c1e11be7df9e8968adc87f89c6c0e6
Gbikex <csatgabor@gmail.com>
2026-04-23 22:32:08 +0200

    Add: page to show task details based on id

---
+++ Web-Development/Playground/React/TaskManager/V2/src/pages/TaskCard.jsx
@@ -0,0 +1,12 @@
+import TaskCardDetails from "../components/TaskCardDetails";
+
+function TaskCard() {
+  return (
+    <>
+      <p>Task Card Details</p>
+      <TaskCardDetails />
+    </>
+  );
+}
+
+export default TaskCard;
