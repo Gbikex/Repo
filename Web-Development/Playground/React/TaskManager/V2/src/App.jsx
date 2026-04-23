ca8349a75bd3a0f13095c400baf308863740996e
Gbikex <csatgabor@gmail.com>
2026-04-23 22:33:44 +0200

    Add: new route for task details based on id

--- Web-Development/Playground/React/TaskManager/V2/src/App.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/App.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import TaskCreate from "./pages/TaskCreate";
 import ProjectCreate from "./pages/ProjectCreate";
+import TaskCard from "./pages/TaskCard";
 
 import { TaskProvider } from "./context/Task";
 import { ProjectProvider } from "./context/Project";
+import { PersonProvider } from "./context/Person";
 import PersonCreate from "./pages/PersonCreate";
 
 function App() {
@@ -14,14 +16,17 @@ function App() {
       <h1>Task Manager</h1>
       <TaskProvider>
         <ProjectProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<Homepage />} />
-              <Route path="/create-task" element={<TaskCreate />} />
-              <Route path="/create-project" element={<ProjectCreate />} />
-              <Route path="/create-person" element={<PersonCreate />} />
-            </Routes>
-          </BrowserRouter>
+          <PersonProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="/create-task" element={<TaskCreate />} />
+                <Route path="/create-project" element={<ProjectCreate />} />
+                <Route path="/create-person" element={<PersonCreate />} />
+                <Route path="/task/:id" element={<TaskCard />} />
+              </Routes>
+            </BrowserRouter>
+          </PersonProvider>
         </ProjectProvider>
       </TaskProvider>
     </>
