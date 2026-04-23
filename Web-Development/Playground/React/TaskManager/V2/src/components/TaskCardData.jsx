de0d4b3fe0d264f11111708476afb094d3ff162e
Gbikex <csatgabor@gmail.com>
2026-04-23 22:31:34 +0200

    Add: new comp for detail of each task based on id

--- Web-Development/Playground/React/TaskManager/V2/src/components/TaskCardData.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/components/TaskCardData.jsx
@@ -1,11 +1,29 @@
+import Button from "./Button";
+
+import { useNavigate } from "react-router-dom";
+
 function TaskCardData({ task }) {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <ul>
-        <li>Task name: {task.taskName}</li>
-        <li>Task priority: {task.priority}</li>
-        <li>Task deadline {task.deadline}</li>
-      </ul>
+      <div>
+        <ul>
+          <li>Task name: {task.taskName}</li>
+          <li>Task priority: {task.priority}</li>
+          <li>Task deadline {task.deadline}</li>
+        </ul>
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            navigate(`/task/${task.id}`);
+          }}
+        >
+          Task
+        </Button>
+        <Button>Project</Button>
+      </div>
     </div>
   );
 }
