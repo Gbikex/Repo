49c8c8816ede720a75b9389a28a2a7bbc590358b
Gbikex <csatgabor@gmail.com>
2026-04-25 19:14:21 +0200

    Add: route to project details

--- Web-Development/Playground/React/TaskManager/V2/src/App.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import TaskCreate from "./pages/TaskCreate";
 import ProjectCreate from "./pages/ProjectCreate";
+import PersonCreate from "./pages/PersonCreate";
+
 import TaskCard from "./pages/TaskCard";
+import ProjectCard from "./pages/ProjectCard";
 
 import { TaskProvider } from "./context/Task";
 import { ProjectProvider } from "./context/Project";
 import { PersonProvider } from "./context/Person";
-import PersonCreate from "./pages/PersonCreate";
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
                 <Route path="/create-project" element={<ProjectCreate />} />
                 <Route path="/create-person" element={<PersonCreate />} />
                 <Route path="/task/:id" element={<TaskCard />} />
+                <Route path="/project/:id" element={<ProjectCard />} />
               </Routes>
             </BrowserRouter>
           </PersonProvider>
