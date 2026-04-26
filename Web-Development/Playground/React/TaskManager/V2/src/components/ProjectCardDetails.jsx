8ab3648d0e226e9b510d434651c367cb414990ad
Gbikex <csatgabor@gmail.com>
2026-04-26 16:34:50 +0200

    Add: fully working update of the avaliable data of the selected project

--- Web-Development/Playground/React/TaskManager/V2/src/components/ProjectCardDetails.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/components/ProjectCardDetails.jsx
@@ -1,27 +1,71 @@
+import { useState } from "react";
+
 import ButtonBack from "./ButtonBack";
+import Button from "./Button";
 
 import { useProject } from "../context/Project";
 import { useParams } from "react-router-dom";
 
 function ProjectCardDetail() {
-  const { projectList } = useProject();
+  const { projectList, dispatch } = useProject();
   const { id } = useParams();
-  console.log(projectList);
-  console.log(id);
 
   const project = projectList.find(
     (project) => Number(project.id) === Number(id),
   );
 
-  console.log(project);
+  const [updatedName, setUpdatedName] = useState(project.projectName);
+  const [updatedDescription, setUpdatedDescription] = useState(
+    project.projectDescription,
+  );
+
+  function handleUpdate() {
+    dispatch({
+      type: "updateProject",
+      payLoad: {
+        id: project.id,
+        projectName: updatedName,
+        projectDescription: updatedDescription,
+      },
+    });
+  }
+
+  function handleReset() {
+    setUpdatedDescription(project.projectName);
+    setUpdatedName(project.projectDescription);
+  }
 
   return (
     <>
       <p>Project Card Detail</p>
-      <p>Project Name: {project.projectName}</p>
-      <p>Project Description: {project.projectDescription}</p>
       <div>
         <ButtonBack />
+        <Button
+          onClick={() => {
+            handleReset();
+          }}
+        >
+          Reset
+        </Button>
+      </div>
+      <p>
+        Project Name:
+        <input
+          value={updatedName}
+          onChange={(e) => setUpdatedName(e.target.value)}
+        ></input>
+      </p>
+      <p>
+        Project Description: {project.projectDescription}
+        <input
+          value={updatedDescription}
+          onChange={(e) => {
+            setUpdatedDescription(e.target.value);
+          }}
+        ></input>
+      </p>
+      <div>
+        <Button onClick={handleUpdate}>Update</Button>
       </div>
     </>
   );
