83b64f7d41a197a01d66c5d6a6215fdb10873ee7
Gbikex <csatgabor@gmail.com>
2026-04-27 22:10:43 +0200

    Add: optional default value, by default always true

--- Web-Development/Playground/React/TaskManager/V2/src/components/SelectListWithTitle.jsx
+++ Web-Development/Playground/React/TaskManager/V2/src/components/SelectListWithTitle.jsx
@@ -1,12 +1,24 @@
 import { DEFAULT_SELECT_PLACEHOLDER } from "../constants/constants";
 
-function SelectListWithTitle({ name, id, value, onChange, isTitle, children }) {
+function SelectListWithTitle({
+  name,
+  id,
+  value,
+  onChange,
+  isTitle,
+  isDefaultOption = true,
+  children,
+}) {
   return (
     <>
       {isTitle !== "" ? <p>{isTitle}</p> : ""}
       <select name={name} id={id} value={value} onChange={onChange}>
         {/*Default select option*/}
-        <option value="">{DEFAULT_SELECT_PLACEHOLDER}</option>
+        {isDefaultOption ? (
+          <option value="">{DEFAULT_SELECT_PLACEHOLDER}</option>
+        ) : (
+          ""
+        )}
         {children}
       </select>
     </>
