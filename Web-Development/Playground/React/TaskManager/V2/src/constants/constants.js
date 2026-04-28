8c8f439a913feaf74fd7c52a439492e65320e609
Gbikex <csatgabor@gmail.com>
2026-04-28 21:19:44 +0200

    Add: task type variable

--- Web-Development/Playground/React/TaskManager/V2/src/constants/constants.js
+++ Web-Development/Playground/React/TaskManager/V2/src/constants/constants.js
@@ -1,3 +1,21 @@
 export const PRIORITY = [1, 2, 3];
 export const STORY_POINTS = [1, 2, 3, 5, 8, 13];
 export const DEFAULT_SELECT_PLACEHOLDER = "-- Please choose an option --";
+export const TASK_TYPE = [
+  {
+    id: 1,
+    type: "Development",
+  },
+  {
+    id: 2,
+    type: "New Feature",
+  },
+  {
+    id: 3,
+    type: "Bug",
+  },
+  {
+    id: 4,
+    type: "Error",
+  },
+];
