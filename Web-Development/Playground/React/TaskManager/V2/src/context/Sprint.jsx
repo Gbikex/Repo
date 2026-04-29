24939fd57418af1d9b31205a9456cb78f772d69e
Gbikex <csatgabor@gmail.com>
2026-04-29 21:01:06 +0200

    Add: skeleton of sprint context

---
+++ Web-Development/Playground/React/TaskManager/V2/src/context/Sprint.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useReducer } from "react";
+
+const SprintContext = createContext();
+
+const initialState = {
+  id: "",
+  sprintName: "",
+  projectId: "",
+  projectName: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "":
+      return { ...state };
+    default:
+      throw new Error("Unknown action 🫥");
+  }
+}
+
+function SprintProvider({ children }) {
+  const [{ id, sprintName, projectId, projectName }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+  return (
+    <SprintContext.Provider
+      value={{
+        id,
+        sprintName,
+        projectId,
+        projectName,
+        dispatch,
+      }}
+    >
+      {children}
+    </SprintContext.Provider>
+  );
+}
+
+function useSprint() {
+  const context = useContext(SprintContext);
+
+  if (context === undefined)
+    throw new Error("The Context was used outside the scope 🤔");
+
+  return context;
+}
+
+export { useSprint, SprintProvider };
